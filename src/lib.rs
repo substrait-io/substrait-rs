7ba4a2f0381124c23567804349f233071a45849e
Andy Grove <andygrove@nvidia.com>
2022-05-27 14:50:54 -0600

    Remove NVIDIA copyright

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,3 @@
-// SPDX-FileCopyrightText: Copyright (c) 2022, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 // SPDX-License-Identifier: Apache-2.0
 
 // include the generated protobuf source as a submodule
