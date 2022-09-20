467c49f33c981ac971f738d7d6db6e09c212d5d7
Matthijs Brobbel <m1brobbel@gmail.com>
2022-09-20 12:46:00 +0200

    Setup GitHub actions and Dependabot

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,20 @@
 // SPDX-License-Identifier: Apache-2.0
 
-// include the generated protobuf source as a submodule
+/// Generated types for the protobuf `substrait` package
 #[allow(clippy::all)]
 pub mod protobuf {
+
+    /// Generated types for the protobuf `substrait.extensions` package
     pub mod extensions {
         include!(concat!(env!("OUT_DIR"), "/substrait.extensions.rs"));
     }
+
     include!(concat!(env!("OUT_DIR"), "/substrait.rs"));
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::protobuf::expression::literal::LiteralType;
-    use crate::protobuf::expression::Literal;
+    use crate::protobuf::expression::{literal::LiteralType, Literal};
 
     #[test]
     fn literal() {
