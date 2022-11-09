43f97ef0e1311bdd909c58afad429b4db54fa30d
Matthijs Brobbel <m1brobbel@gmail.com>
2022-11-09 02:29:25 +0100

    Add `pbjson` feature for Protobuf JSON serde (#6)
    * Add pbjson feature for Protobuf JSON serde
    
    * Bump pbjson from 0.4 to 0.5
    
    * Fix docs workflow concurrency

--- src/lib.rs
+++ src/lib.rs
@@ -7,14 +7,22 @@ pub mod protobuf {
     /// Generated types for the protobuf `substrait.extensions` package
     pub mod extensions {
         include!(concat!(env!("OUT_DIR"), "/substrait.extensions.rs"));
+
+        #[cfg(feature = "pbjson")]
+        include!(concat!(env!("OUT_DIR"), "/substrait.extensions.serde.rs"));
     }
 
     include!(concat!(env!("OUT_DIR"), "/substrait.rs"));
+
+    #[cfg(feature = "pbjson")]
+    include!(concat!(env!("OUT_DIR"), "/substrait.serde.rs"));
 }
 
 #[cfg(test)]
 mod tests {
     use crate::protobuf::expression::{literal::LiteralType, Literal};
+    #[cfg(feature = "pbjson")]
+    use std::error::Error;
 
     #[test]
     fn literal() {
@@ -23,5 +31,35 @@ mod tests {
             literal_type: Some(LiteralType::I32(123)),
             type_variation_reference: 0,
         };
+    }
+
+    #[cfg(feature = "pbjson")]
+    #[test]
+    fn pbjson_serde() -> Result<(), Box<dyn Error>> {
+        let plan: crate::protobuf::Plan = serde_json::from_str(
+            r#"{
+                "relations": [
+                    {
+                        "root": {
+                           "names": ["substrait"]
+                        }
+                    },
+                    {
+                        "rel": {
+                            "filter": {
+                                "condition": {
+                                    "literal": {
+                                        "nullable": true,
+                                        "boolean": true
+                                    }
+                                }
+                            }
+                        }
+                    }
+                ]
+            }"#,
+        )?;
+        assert_eq!(plan.relations.len(), 2);
+        Ok(())
     }
 }
