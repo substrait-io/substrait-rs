e56e646c3c672daaf39d3ce7676acbbbfc4e0d84
Matthijs Brobbel <m1brobbel@gmail.com>
2024-07-10 09:41:51 +0200

    docs: `--cfg=docsrs` is now a default set by docs.rs (#190)
    It's no longer required to specify this in our package metadata.

--- src/parse/proto/extensions/mod.rs
+++ src/parse/proto/extensions/mod.rs
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: Apache-2.0
 
-//! Parsing of [proto::extensions] types.
+//! Parsing of [crate::proto::extensions] types.
 
 mod simple_extension_uri;
 pub use simple_extension_uri::SimpleExtensionUri;
